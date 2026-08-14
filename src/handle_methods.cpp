9831365012c60c1892e5959fa08ec6ff08377e05
Raymond Burkholder <raymond@burkholder.net>
2026-08-14 00:14:30 +0000

    src/handle_methods.cpp - add clacks overhead

--- src/handle_methods.cpp
+++ src/handle_methods.cpp
@@ -27,44 +27,50 @@
 
 namespace {
   static const std::string c_sVersion( "ounl web server with lua v1.0");
+
+  template<typename Response>
+  void response_common( Response& response ) {
+    response.set( http::field::server, c_sVersion );
+    response.set( "x-clacks-overhead", "GNU Terry Pratchett" );
+  }
 }
 
 void response_bad_request( response_t& response, const boost::beast::string_view why ) {
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
   response.set( http::field::content_type, "text/plain");
   response.body() = std::string( why );
 }
 
 void response_not_found( response_t& response, const boost::beast::string_view target ) {
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
   response.set( http::field::content_type, "text/plain");
   response.body() = "The resource '" + std::string( target ) + "' was not found.\n";
 }
 
 void response_server_error( response_t& response, const boost::beast::string_view what ) {
-  response.set( http::field::server, c_sVersion);
+  response_common( response );
   response.set( http::field::content_type, "text/plain");
   response.body() = "An error occurred: '" + std::string( what ) + "'\n";
 }
 
 void resource_robots_txt( response_t& response ) {
   static const std::string content( "User-agent: *\nAllow: /\n" );
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
   //response.set( http::field::content_type, "text/html");
   response.body() = content;
   response.content_length( content.size() );
 }
 
 void method_head( http::response<http::empty_body>& response ) {
-  response.set( http::field::server, c_sVersion);
+  response_common( response );
 }
 
 void method_get( http::response<http::file_body>& response ) {
-  response.set( http::field::server, c_sVersion);
+  response_common( response );
 }
 
 void method_post( response_t& response ) {
-  response.set( http::field::server, c_sVersion);
+  response_common( response );
   response.set( http::field::content_type, "text/plain");
   response.body() = "your post was accepted\n";
 }
@@ -76,7 +82,7 @@ void lua_method_get( std::string& path, sol_lua_t& sol_lua, http::response<http:
 
   sol_lua().open_libraries( sol::lib::base, sol::lib::package, sol::lib::table, sol::lib::string );
 
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
 
   sol_lua().set_function(
     "Render",
@@ -118,7 +124,7 @@ void lua_method_head( std::string& path, sol_lua_t& sol_lua, http::response<http
 
   //sol_lua.m_sol.open_libraries( sol::lib::base, sol::lib::package, sol::lib::table, sol::lib::string );
 
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
 
   sol::load_result script;
   try {
@@ -143,7 +149,7 @@ void lua_method_post( std::string& path, sol_lua_t& sol_lua, http::response<http
 
   sol_lua().open_libraries( sol::lib::base, sol::lib::package, sol::lib::table, sol::lib::string );
 
-  response.set( http::field::server, c_sVersion );
+  response_common( response );
 
   sol_lua().set_function(
     "Render",
